34dbf89975eb9969fe7edee86ce14faa67c21b7a
jordan-m-wells <70729861+jordan-m-wells@users.noreply.github.com>
2022-05-09 07:35:39 -0400

    Update getting_user_tweets.py
    Added description and comments.

--- getting_user_tweets.py
+++ getting_user_tweets.py
@@ -1,9 +1,10 @@
 """
-Author: Jacob Krawitz,
+Author: Jacob Krawitz, Jordan Wells
 Date: 5/9/22
 Muhlenberg College 2022, Computer Science CUE
 
-Description:
+Description: In this file, the timeline tweets of the users which liked the given horoscope 
+content are collected. 
 
 """
 
@@ -51,8 +52,11 @@ Method: main()
 
 Description: 
 
-@params 
-@returns 
+In this method, timeline tweets for users which liked given horoscope content
+are collected and added to a .json file in a directory for each user. 
+
+@params none
+@returns none
 
 '''
 
@@ -60,6 +64,7 @@ Description:
 def main():
     user_num_id = 0
 
+    # open and load liked users .json file
     with open('./jsons/liked_users/'+sign+'_liked_users.json') as f:
 
         f_data = json.load(f)
@@ -73,28 +78,34 @@ def main():
             user_name = user['user_name']
             user_id = user['id']
 
+             # collect up to 50 of the users' timeline tweets excluding retweets
             tweets = tweepy.Paginator(client.get_users_tweets, id=user_id, exclude=['retweets', 'replies'],
                                       tweet_fields=['lang'], max_results=50).flatten(limit=50)
 
             if tweets is not None:
                 for tweet in tweets:
+                    # make sure tweet exsists
                     if len(tweet.text) > 0:
+                        # make sure tweet is in english
                         if tweet.lang == 'en':
                             obj = {'user_num_id': user_num_id, 'user_id': user_id, 'author': user_name, 'tweet_num': tweet_number, 'tweet_id': tweet.id,
                                    'lang': tweet.lang, 'text': tweet.text}
+                            # append user information and tweet to file
                             results.append(obj)
+                            # count the number of tweets
                             tweet_number += 1
                     else:
                         print("Empty!")
 
-            # Make directory for each user
+            # make directory for each user
             if not os.path.exists('./jsons/users_timeline_tweets/'+sign+'/'+str(user_num_id)):
                 os.makedirs('./jsons/users_timeline_tweets/'+sign+'/'+str(user_num_id))
 
             with open('./jsons/users_timeline_tweets/'+sign+'/'+str(user_num_id)+'/'+str(user_num_id)+'.json', 'w+') as out:
                 json.dump(results, out, indent=4)
 
-            if user_num_id == 1000: # 5000 users 10 max tweets
+            # limit set to 1000 users
+            if user_num_id == 1000: 
                 return
 
             user_num_id += 1
