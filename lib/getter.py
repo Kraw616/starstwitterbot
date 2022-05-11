c1e960ffc7ddf2c4515a67e24df7f72dcfbb7d5d
jordan-m-wells <70729861+jordan-m-wells@users.noreply.github.com>
2022-05-10 23:47:11 -0400

    Added good comments

--- lib/getter.py
+++ lib/getter.py
@@ -1,13 +1,19 @@
 """
+
+
 Author: Jacob Krawitz, Jordan Wells, Alek Demaio
+
 Date: 5/10/22
+
 Muhlenberg College 2022, Computer Science CUE
 
 Description:
 
 In this file, tweet data is collected from horoscope accounts located in a .csv file.
 
+
 """
+
 
 # import statements
 import pandas as pd
@@ -30,20 +36,27 @@ In this method, the user id is taken from the data frame of a given sign.
 
 def get_accounts(client, data_frame, sign):
 
+    # create list for user ids
     user_ids = []
 
+    # add accounts in data frame to list
     accounts = data_frame[sign].tolist()
 
     # get user information from data frame 
     for account in accounts:
         user = client.get_user(username=account)
         user_id = user.data.id
+        
+        # append the user id to the file
         user_ids.append(user_id)
 
+    # return user ids
     return user_ids
 
 
 '''
+
+
 Method: recent_tweets()
 
 Description: 
@@ -53,11 +66,13 @@ In this method, tweets are collected from the user ids, with a maximum of 500 ho
 @params user ids, client, and sign
 @returns horoscope tweets .json file
 
+
 '''
 
 
 def recent_tweets(user_ids, client, sign):
 
+    # create list for results
     results = []
 
     for user_id in user_ids:
@@ -79,6 +94,20 @@ def recent_tweets(user_ids, client, sign):
     # write new tweet .json file
     with open('./jsons/horoscope_tweets/'+sign+'_tweets.json', 'w+', encoding='utf-8') as f:
         json.dump(results, f, indent=4)
+        
+   
+'''
+Method: main()
+
+Description: 
+
+In this method, the recent_tweets and get_accounts methods are called.
+
+@params client, data_frame, sign
+@returns recent tweets from horoscope accounts
+        
+
+'''
 
 
 def main(client, data_frame, sign):
